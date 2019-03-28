834bb0f91977fdb9068347a7ea1d9d23ccdb162f
joeyism <sham.joey@gmail.com>
2019-03-28 01:05:02 -0400

    Added ping10, which just does it for 10 tries then outputs

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
 outfile=jping
 gcc ping.c -o $outfile
+sudo chown root:root ./$outfile
+sudo setcap cap_net_raw+ep ./$outfile
+sudo mv $outfile /usr/bin/
+
+
+outfile=jping10
+gcc ping10.c -o $outfile
 sudo chown root:root ./$outfile
 sudo setcap cap_net_raw+ep ./$outfile
 sudo mv $outfile /usr/bin/
